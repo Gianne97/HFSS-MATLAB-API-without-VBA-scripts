873b4a01073f2a5ae66ff883dcb740acd4781cbf
Giacomo Giannetti <73080596+Gianne97@users.noreply.github.com>
2023-06-22 09:22:54 +0200

    Add files via upload

---
+++ PatchAntenna/ExportToFile_Sparam.py
@@ -0,0 +1,8 @@
+oDesktop.RestoreWindow()
+oProject = oDesktop.SetActiveProject("Modified")
+oDesign = oProject.SetActiveDesign("Design")
+oModule = oDesign.GetModule("ReportSetup")
+oModule.UpdateReports(["S11mag"])
+oModule.ExportToFile("S11mag", "C:\Users\giannetti\Documents\HFSS-MATLAB_interface\Dev5\TrapezoidalFeed\S11mag.csv")
+oModule.UpdateReports(["S11pha"])
+oModule.ExportToFile("S11pha", "C:\Users\giannetti\Documents\HFSS-MATLAB_interface\Dev5\TrapezoidalFeed\S11pha.csv")
