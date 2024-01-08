097195c3b19bd28778b6cef0646bff9bbad3af70
Giacomo Giannetti <73080596+Gianne97@users.noreply.github.com>
2024-01-08 14:54:27 +0100

    Delete PatchAntenna_AutomaticSignpostGeneration/ExportToFile_Sparam.py

--- PatchAntenna_AutomaticSignpostGeneration/ExportToFile_Sparam.py
+++
@@ -1,8 +0,0 @@
-oDesktop.RestoreWindow()
-oProject = oDesktop.SetActiveProject("Modified")
-oDesign = oProject.SetActiveDesign("Design")
-oModule = oDesign.GetModule("ReportSetup")
-oModule.UpdateReports(["S11mag"])
-oModule.ExportToFile("S11mag", "C:\Users\giannetti\Documents\HFSS-MATLAB_interface\Dev6\S11mag.csv")
-oModule.UpdateReports(["S11pha"])
-oModule.ExportToFile("S11pha", "C:\Users\giannetti\Documents\HFSS-MATLAB_interface\Dev6\S11pha.csv")
